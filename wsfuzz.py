e6d85a5751328d37da8ce4a9da0fec882677ce88
bombunx <40179377+bombunx@users.noreply.github.com>
2023-06-28 04:08:59 +0000

    Added basic payloads. Created custom argparse for easier test cases. Uncomment your part for testing and comment out the others.

--- wsfuzz.py
+++ wsfuzz.py
@@ -1,25 +1,83 @@
 from argparse import ArgumentParser
 import wsHandler as w
+import sys
+
 
 def args():
     parser = ArgumentParser()
-    parser.add_argument('-t', '--target', nargs='?', type=str, required=True, help='Target Websocket URL')
-    parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi, lfi'], required=True, help='Type of attack to carry out')
-    parser.add_argument('-r', '--request', nargs='?', type=str, required=True, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}")
-    parser.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use')
+    
+    # Uncomment this
+    # parser.add_argument('-t', '--target', nargs='?', type=str, required=True, help='Target Websocket URL')
+    # parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi, lfi'], required=True, help='Type of attack to carry out')
+    # parser.add_argument('-r', '--request', nargs='?', type=str, required=True, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}")
+    # parser.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use')
+    
+    # Testing Purpose (sqli)
+    # parser.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/authenticate-user')
+    # parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi, lfi'], help='Type of attack to carry out', default='sqli')
+    # parser.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="{'auth_user'':'*','auth_pass':'*'}")
+    # parser.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/sqli.txt')
+    # return parser.parse_args()
+
+    # # Testing Purpose (xss)
+    # parser.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/reflected-xss')
+    # parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi, lfi'], help='Type of attack to carry out', default='xss')
+    # parser.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="{'*'}")
+    # parser.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/xss.txt')
+    # return parser.parse_args()
+
+    # # Testing Purpose (cli)
+    # parser.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/command-execution')
+    # parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi, lfi'], help='Type of attack to carry out', default='cmdi')
+    # parser.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="{'*'}")
+    # parser.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/cmdi.txt')
+    # return parser.parse_args()
+
+    # Testing Purpose (lfi)
+    parser.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/file-inclusion')
+    parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi, lfi'], help='Type of attack to carry out', default='lfi')
+    parser.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="{'*'}")
+    parser.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/lfi.txt')
     return parser.parse_args()
 
 
-def xss(target, payload, exampleRequest):
-    print('xss')
 
+
+
+def xss(target, payload, exampleRequest):
+    with open (payload, 'r') as payload_file:
+        payloads = payload_file.readlines()
+    for line in payloads:
+        line = line.replace('\n', '')
+        newRequest = exampleRequest.replace('*', line)
+        w.InteractWithWsSite(target, newRequest)
+    print('xss')
+    
 def lfi(target, payload, exampleRequest):
+    with open (payload, 'r') as payload_file:
+        payloads = payload_file.readlines()
+    for line in payloads:
+        line = line.replace('\n', '')
+        newRequest = exampleRequest.replace('*', line)
+        w.InteractWithWsSite(target, newRequest)
     print('lfi')
 
 def sqli(target, payload, exampleRequest):
+    with open (payload, 'r') as payload_file:
+        payloads = payload_file.readlines()
+    for line in payloads:
+        line = line.replace('\n', '')
+        newRequest = exampleRequest.replace('*', line)
+        w.InteractWithWsSite(target, newRequest)
     print('sqli')
-
+    
 def cmdi(target, payload, exampleRequest):
+    with open (payload, 'r') as payload_file:
+        payloads = payload_file.readlines()
+    for line in payloads:
+        line = line.replace('\n', '')
+        newRequest = exampleRequest.replace('*', line)
+        w.InteractWithWsSite(target, newRequest)
     print('cmd injection')
 
 
@@ -58,7 +116,6 @@ def main():
         else:
             payload = "dir_to_default_cmi_payload"
         cmdi(arg.target, payload, arg.request)
-
 
 
 if __name__ == '__main__':
