135c62519ff04bf71dd75d9128c01513d95b02e1
Tang Zhi Xiang <zifengtang540@gmail.com>
2023-06-29 16:07:45 +0800

    readable output messages

--- wsfuzz.py
+++ wsfuzz.py
@@ -2,6 +2,13 @@ from argparse import ArgumentParser
 import wsHandler as w
 import sys
 import base64
+from colorama import init, Fore
+
+# coloura initialization
+init()
+GREEN = Fore.GREEN
+RED = Fore.RED
+RESET = Fore.RESET
 
 
 def args():
@@ -9,27 +16,27 @@ def args():
     
     # Uncomment this
     # parser.add_argument('-t', '--target', nargs='?', type=str, required=True, help='Target Websocket URL')
-    # parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi, lfi'], required=True, help='Type of attack to carry out')
+    # parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi, 'lfi'], required=True, help='Type of attack to carry out')
     # parser.add_argument('-r', '--request', nargs='?', type=str, required=True, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}")
     # parser.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use')
     
     # Testing Purpose (sqli)
     parser.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/authenticate-user-blind')
-    parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi, lfi'], help='Type of attack to carry out', default='sqli')
+    parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi' , 'lfi'], help='Type of attack to carry out', default='sqli')
     parser.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default='{"auth_user":"*","auth_pass":""}')
     parser.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/sqli.txt')
     return parser.parse_args()
 
     # # Testing Purpose (xss)
     # parser.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/reflected-xss')
-    # parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi, lfi'], help='Type of attack to carry out', default='xss')
+    # parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi', 'lfi'], help='Type of attack to carry out', default='xss')
     # parser.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="{'*'}")
     # parser.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/xss.txt')
     # return parser.parse_args()
 
     # # Testing Purpose (cli)
     # parser.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/command-execution')
-    # parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi, lfi'], help='Type of attack to carry out', default='cmdi')
+    # parser.add_argument('-a', '--attack', choices=['xss', 'sqli', 'cmdi', 'lfi'], help='Type of attack to carry out', default='cmdi')
     # parser.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="{'*'}")
     # parser.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/cmdi.txt')
     # return parser.parse_args()
@@ -46,6 +53,7 @@ def args():
 
 
 def xss(target, payload, exampleRequest):
+    print(f"{GREEN}[+]{RESET} xss selected! Commencing XSS attack...")
     with open (payload, 'r') as payload_file:
         payloads = payload_file.readlines()
     for line in payloads:
@@ -57,6 +65,7 @@ def xss(target, payload, exampleRequest):
 
 
 def lfi(target, payload, exampleRequest):
+    print(f"{GREEN}[+]{RESET} lfi selected! Commencing LFI attack...")
     with open (payload, 'r') as payload_file:
         payloads = payload_file.readlines()
     for line in payloads:
@@ -68,6 +77,7 @@ def lfi(target, payload, exampleRequest):
 
 
 def sqli(target, payload, exampleRequest):
+    print(f"{GREEN}[+]{RESET} sqli selected! Commencing SQLi attack...")
     with open (payload, 'r') as payload_file:
         payloads = payload_file.readlines()
     for line in payloads:
@@ -83,6 +93,7 @@ def sqli(target, payload, exampleRequest):
 
 
 def cmdi(target, payload, exampleRequest):
+    print(f"{GREEN}[+]{RESET} cmdi selected! Commencing command injection attack...")
     with open (payload, 'r') as payload_file:
         payloads = payload_file.readlines()
     for line in payloads:
@@ -97,7 +108,7 @@ def main():
     arg = args()
 
     if arg.target[0:5] != "ws://":
-        print("Please enter a valid websocket url")
+        print(f"{RED}[-]{RESET} Please enter a valid websocket url")
         exit(0)
 
     if arg.attack == 'xss':
@@ -111,4 +122,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(f"{RED}[-]{RESET} User initiated termination, exiting...")
+    except:
+        print(f"{RED}[-]{RESET} Unexpected errors occurred, exiting...")
