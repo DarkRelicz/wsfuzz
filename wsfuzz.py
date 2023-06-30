1ea53804bd06125e938969d0e5b38a3829b1f843
Tang Zhi Xiang <zifengtang540@gmail.com>
2023-07-01 01:29:42 +0800

    added support for encoding schemes

--- wsfuzz.py
+++ wsfuzz.py
@@ -11,6 +11,8 @@ RED = Fore.RED
 RESET = Fore.RESET
 
 
+
+# retrieves command line arguments entered
 def args():
     parser = ArgumentParser()
 
@@ -23,25 +25,49 @@ def args():
     xss.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/reflected-xss')
     xss.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="{'*'}")
     xss.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/xss.txt')
+    xss.add_argument('-e', '--encode', nargs='?', type=str, choices=('normal', 'base64'), help='Encoding scheme to use, select normal for no encoding', default='normal')
 
     sqli.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/authenticate-user-blind')
     sqli.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default='{"auth_user":"*","auth_pass":""}')
     sqli.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/sqli.txt')
+    sqli.add_argument('-e', '--encode', nargs='?', type=str, choices=('normal', 'base64'), help='Encoding scheme to use, select normal for no encoding', default='base64')
 
     cmdi.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/command-execution')
     cmdi.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="{'*'}")
     cmdi.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/cmdi.txt')
+    cmdi.add_argument('-e', '--encode', nargs='?', type=str, choices=('normal', 'base64'), help='Encoding scheme to use, select normal for no encoding', default='normal')
 
     lfi.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/file-inclusion')
     lfi.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="{'*'}")
     lfi.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/lfi.txt')
+    lfi.add_argument('-e', '--encode', nargs='?', type=str, choices=('normal', 'base64'), help='Encoding scheme to use, select normal for no encoding', default='normal')
 
     return parser.parse_args()
 
 
 
-def xss(target, payload, exampleRequest):
+# return encoded messages based on the selected scheme
+def encoding(encoding_scheme, msg):
+    if encoding_scheme == 'base64':
+        return b64(msg)
+    elif encoding_scheme == 'normal':
+        return msg
+
+
+
+# perform base64 encoding
+def b64(payload):
+    msg = payload.encode('utf-8')
+    b64_str = base64.b64encode(msg)
+    msg = b64_str.decode('utf-8')
+    return msg
+
+
+
+# conduct cross site scripting attack
+def xss(target, payload, exampleRequest, encode):
     print(f"{GREEN}[+]{RESET} xss selected! Commencing XSS attack...")
+    print(f"{GREEN}[+]{RESET} {encode} encoding scheme detected!")
     with open (payload, 'r') as payload_file:
         payloads = payload_file.readlines()
     for line in payloads:
@@ -52,8 +78,10 @@ def xss(target, payload, exampleRequest):
     
 
 
-def lfi(target, payload, exampleRequest):
+# conduct local file inclusion attack
+def lfi(target, payload, exampleRequest, encode):
     print(f"{GREEN}[+]{RESET} lfi selected! Commencing LFI attack...")
+    print(f"{GREEN}[+]{RESET} {encode} encoding scheme detected!")
     with open (payload, 'r') as payload_file:
         payloads = payload_file.readlines()
     for line in payloads:
@@ -64,24 +92,25 @@ def lfi(target, payload, exampleRequest):
 
 
 
-def sqli(target, payload, exampleRequest):
+# conduct sql injection attack
+def sqli(target, payload, exampleRequest, encode):
     print(f"{GREEN}[+]{RESET} sqli selected! Commencing SQLi attack...")
+    print(f"{GREEN}[+]{RESET} {encode} encoding scheme detected!")
     with open (payload, 'r') as payload_file:
         payloads = payload_file.readlines()
     for line in payloads:
         line = line.replace('\n', '')
         print("payload: %s" % line)
-        msg = line.encode('utf-8')
-        b64 = base64.b64encode(msg)
-        msg = b64.decode('utf-8')
-        newRequest = exampleRequest.replace('*', msg)
+        newRequest = exampleRequest.replace('*', encoding(encode, line))
         response = w.InteractWithWsSite(target, newRequest)
         print("response: %s\n" % response)
     
 
 
-def cmdi(target, payload, exampleRequest):
+# conduct command injection attack
+def cmdi(target, payload, exampleRequest, encode):
     print(f"{GREEN}[+]{RESET} cmdi selected! Commencing command injection attack...")
+    print(f"{GREEN}[+]{RESET} {encode} encoding scheme detected!")
     with open (payload, 'r') as payload_file:
         payloads = payload_file.readlines()
     for line in payloads:
@@ -95,18 +124,19 @@ def cmdi(target, payload, exampleRequest):
 def main():
     arg = args()
 
-    if arg.target[0:5] != "ws://":
+    if arg.target[0:5] != "ws://" and arg.target[0:6] != "wss://":
         print(f"{RED}[-]{RESET} Please enter a valid websocket url")
         exit(0)
 
     if arg.attack == 'xss':
-        xss(arg.target, arg.payload, arg.request)
+        xss(arg.target, arg.payload, arg.request, arg.encode)
     if arg.attack == 'lfi':
-        lfi(arg.target, arg.payload, arg.request)
+        lfi(arg.target, arg.payload, arg.request, arg.encode)
     if arg.attack == 'sqli':
-        sqli(arg.target, arg.payload, arg.request)
+        sqli(arg.target, arg.payload, arg.request, arg.encode)
     if arg.attack == 'cmdi':
-        cmdi(arg.target, arg.payload, arg.request)
+        cmdi(arg.target, arg.payload, arg.request, arg.encode)
+
 
 
 if __name__ == '__main__':
@@ -114,5 +144,3 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         print(f"{RED}[-]{RESET} User initiated termination, exiting...")
-    except:
-        print(f"{RED}[-]{RESET} Unexpected errors occurred, exiting...")
