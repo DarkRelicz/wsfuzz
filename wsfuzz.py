6ed29bc2d07d9e41ffddeb1ff9f66b8686941cfc
bombunx <40179377+bombunx@users.noreply.github.com>
2023-07-09 19:11:15 +0800

    Simplified argparse
    Added more encoding techniques

--- wsfuzz.py
+++ wsfuzz.py
@@ -6,7 +6,7 @@ from progressbar import progressbar
 
 import wsHandler as w
 
-# coloura initialization
+# Colours initialization
 init()
 GREEN = Fore.GREEN
 RED = Fore.RED
@@ -16,49 +16,42 @@ ERROR_LIST = ["invalid", "could not", "can't", "cannot", "error", "incorrect, 'C
 KEYWORDS = ["wsfuzz"]
 
 
-# retrieves command line arguments entered
+# Retrieves command line arguments entered
 def args():
     parser = ArgumentParser()
-
     subparser = parser.add_subparsers(title='Attack', dest='attack', help='Type of attack to carry out', required=True)
-    xss = subparser.add_parser('xss')
-    sqli = subparser.add_parser('sqli')
-    cmdi = subparser.add_parser('cmdi')
-    lfi = subparser.add_parser('lfi')
 
-    xss.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/reflected-xss')
-    xss.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="*")
-    xss.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/xss.txt')
-    xss.add_argument('-e', '--encode', nargs='?', type=str, choices=('normal', 'base64'), help='Encoding scheme to use, select normal for no encoding', default='normal')
+    attack_args = [
+        ('xss', 'ws://dvws.local:8080/reflected-xss', 'payloads/xss.txt'),
+        ('sqli', 'ws://dvws.local:8080/authenticate-user-blind', 'payloads/sqli.txt'),
+        ('cmdi', 'ws://dvws.local:8080/command-execution', 'payloads/cmdi.txt'),
+        ('lfi', 'ws://dvws.local:8080/file-inclusion', 'payloads/lfi.txt')
+    ]
 
-    sqli.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/authenticate-user-blind')
-    sqli.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default='*')
-    sqli.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/sqli.txt')
-    sqli.add_argument('-e', '--encode', nargs='?', type=str, choices=('normal', 'base64'), help='Encoding scheme to use, select normal for no encoding', default='normal')
-
-    cmdi.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/command-execution')
-    cmdi.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="*")
-    cmdi.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/cmdi.txt')
-    cmdi.add_argument('-e', '--encode', nargs='?', type=str, choices=('normal', 'base64'), help='Encoding scheme to use, select normal for no encoding', default='normal')
-
-    lfi.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default='ws://dvws.local:8080/file-inclusion')
-    lfi.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="*")
-    lfi.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default='payloads/lfi.txt')
-    lfi.add_argument('-e', '--encode', nargs='?', type=str, choices=('normal', 'base64'), help='Encoding scheme to use, select normal for no encoding', default='normal')
+    # Set default values for different attacks; users can supply their own parameters
+    for attack, default_target, default_payload in attack_args:
+        attack_parser = subparser.add_parser(attack)
+        attack_parser.add_argument('-t', '--target', nargs='?', type=str, help='Target Websocket URL', default=default_target)
+        attack_parser.add_argument('-r', '--request', nargs='?', type=str, help="Format of an example request, e.g. {'auth_user'':'*','auth_pass':'*'}", default="*")
+        attack_parser.add_argument('-p', '--payload', nargs='?', type=str, help='Payload file to use', default=default_payload)
+        attack_parser.add_argument('-e', '--encode', nargs='?', type=str, choices=('none', 'base64', 'hex', 'url'), help='Encoding scheme to use, default=none ', default='none')
 
     return parser.parse_args()
 
 
-
-# return encoded messages based on the selected scheme
+# Return encoded messages based on the selected scheme
 def encoding(encoding_scheme, msg):
     if encoding_scheme == 'base64':
         return base64.b64encode(msg.encode('utf-8')).decode('utf-8')
-    elif encoding_scheme == 'normal':
-        return msg
+    elif encoding_scheme == 'hex':
+        return msg.encode('utf-8').hex()
+    elif encoding_scheme == 'url':
+        return urllib.parse.quote_plus(msg)
+    # More methods will be supported in the future
+    return msg
 
 
-# encode and send payloads to server
+# Encode and send payloads to server
 def test_payloads(payload_list, exampleRequest, target, encode, attack_name):
     for payload in progressbar((payload_list), redirect_stdout=True):
         line = payload.strip('\n')
@@ -66,7 +59,6 @@ def test_payloads(payload_list, exampleRequest, target, encode, attack_name):
         response = w.InteractWithWsSite(target, newRequest)
         global PAYLOAD 
         PAYLOAD = line
-        # print(response)
         if check_response(response):
             print(f"{GREEN}[+]{RESET} {attack_name} successful!")
             print(f"{GREEN}[+]{RESET} Payload: %s" % line)
@@ -74,9 +66,11 @@ def test_payloads(payload_list, exampleRequest, target, encode, attack_name):
         else:
             print(f"{RED}[-]{RESET} {attack_name} failed!")
             print(f"{RED}[-]{RESET} Payload: %s\n" % line)
-            
+
+
+# Different checks to test if response is a valid/successful attack
 def check_response(response):
-    if response == '':
+    if not response:
         return False
     if response == ' ':
         return False
@@ -88,86 +82,7 @@ def check_response(response):
         return True
     return True
 
-# # conduct cross site scripting attack
-# def xss(target, payload, exampleRequest, encode):
-#     print(f"{GREEN}[+]{RESET} xss selected! Commencing XSS attack...")
-#     print(f"{GREEN}[+]{RESET} {encode} encoding scheme detected!")
-#     with open(payload, 'r') as payload_file:
-#         payloads = payload_file.readlines()
-#     for line in payloads:
-#         line = line.replace('\n', '')
-#         newRequest = exampleRequest.replace('*', line)
-#         response = w.InteractWithWsSite(target, newRequest)
-#         print("response: %s\n" % response)
-    
-
-
-# # conduct local file inclusion attack
-# def lfi(target, payload, exampleRequest, encode):
-#     print(f"{GREEN}[+]{RESET} lfi selected! Commencing LFI attack...")
-#     print(f"{GREEN}[+]{RESET} {encode} encoding scheme detected!")
-#     with open (payload, 'r') as payload_file:
-#         payloads = payload_file.readlines()
-#     for line in payloads:
-#         line = line.replace('\n', '')
-#         newRequest = exampleRequest.replace('*', line)
-#         response = w.InteractWithWsSite(target, newRequest)
-#         if (response != '' and response != ' ') or any([x in response.casefold() for x in ERROR_LIST]):
-#             print(f"{GREEN}[+]{RESET} Local file inclusion successful!")
-#             print(f"{GREEN}[+]{RESET} Payload: %s" % line)
-#             print(f"{GREEN}[+]{RESET} Response: %s\n" % response)
-#         else:
-#             print(f"{RED}[-]{RESET} Local file inclusion failed!")
-#             print(f"{RED}[-]{RESET} Payload: %s\n" % line)
-
-
-
-# # conduct sql injection attack
-# def sqli(target, payload, exampleRequest, encode):
-    
-#     print(f"{GREEN}[+]{RESET} sqli selected! Commencing SQLi attack...")
-#     print(f"{GREEN}[+]{RESET} {encode} encoding scheme detected!")
-#     with open (payload, 'r') as payload_file:
-#         payloads = payload_file.readlines()
-#     for line in payloads:
-#         line = line.replace('\n', '')
-#         newRequest = exampleRequest.replace('*', encoding(encode, line))
-#         print("request: %s\n" % newRequest)
-#         response = w.InteractWithWsSite(target, newRequest)
-#         print("response: %s\n" % response)
-#         if response != ' ':
-#         # chk_match = [x for x in error_list if x in response.casefold()]
-#         if any([x in response.casefold() for x in ERROR_LIST]) or response == ' ':
-#             print(f"{RED}[-]{RESET} SQL injection failed!")
-#             print(f"{RED}[-]{RESET} Payload: %s\n" % line)
-#         else:
-#             print(f"{GREEN}[+]{RESET} SQL injection successful!")
-#             print(f"{GREEN}[+]{RESET} Payload: %s" % line)
-#             print(f"{GREEN}[+]{RESET} Response: %s\n" % response)
-# # usage
-# # python wsfuzz.py sqli -r '{"auth_user":"*","auth_pass":""}' -p payloads/custom_sqli.txt -e base64    
-
-
-# # conduct command injection attack
-# def cmdi(target, payload, exampleRequest, encode):
-#     print(f"{GREEN}[+]{RESET} cmdi selected! Commencing command injection attack...")
-#     print(f"{GREEN}[+]{RESET} {encode} encoding scheme detected!")
-#     with open (payload, 'r') as payload_file:
-#         payloads = payload_file.readlines()
-#     for line in payloads:
-#         line = line.replace('\n', '')
-#         newRequest = exampleRequest.replace('*', line)
-#         response = w.InteractWithWsSite(target, newRequest)
-#         if 'wsfuzz' in response:
-#             print(f"{GREEN}[+]{RESET} Command injection successful!")
-#             print(f"{GREEN}[+]{RESET} Command output: {response}")
-#             print(f"{GREEN}[+]{RESET} Command: {line}")
-#         else:
-#             print(f"{RED}[-]{RESET} Command injection failed!")
-#             print(f"{RED}[-]{RESET} Command: {line}")
-#         print("response: %s\n" % response)
-#         sleep(0.001)
-
+# Start attack
 def execute_attack(target, payload, example_request, encode, attack_name):
     print(f"{GREEN}[+]{RESET} {attack_name} selected! Commencing {attack_name} attack...")
     print(f"{GREEN}[+]{RESET} {encode} encoding scheme detected!")
@@ -175,9 +90,15 @@ def execute_attack(target, payload, example_request, encode, attack_name):
         payload_list = payload_file.readlines()
     test_payloads(payload_list, example_request, target, encode, attack_name)
     
-# usage
-# python wsfuzz.py sqli -r '{"auth_user":"*","auth_pass":""}' -p payloads/custom_sqli.txt -e base64
-# python wsfuzz.py cmdi -t ws://dvws.local:8080/command-execution -r "127.0.0.1*" -p payloads/cmdi2.txt -e normal    
+# usage (full)
+# python wsfuzz.py {attack_name} -t {target_url} -r {payload_example_string} -p {payload_list} -e {encoding_method}
+# python wsfuzz.py sqli -t ws://dvws.local:8080/authenticate-user -r '{"auth_user":"*","auth_pass":""}' -p payloads/custom_sqli.txt -e base64
+# python wsfuzz.py cmdi -t ws://dvws.local:8080/command-execution -r "127.0.0.1*" -p payloads/cmdi2.txt -e normal
+# python wsfuzz.py lfi -t ws://dvws.local:8080/file-inclusion -r "*" -p payloads/lfi.txt -e normal    
+# python wsfuzz.py xss -t ws://dvws.local:8080/reflected-xss -r "*" -p payloads/cmdi2.txt -e normal
+
+# usage (partial)
+# python wsfuzz.py xss -t ws://dvws.local:8080/file-inclusion -r "*" 
 
 def main():
     arg = args()
